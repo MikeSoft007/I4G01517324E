4ac718abdc1dc331fbb453b56edd766b46f99a72
MikeSoft007 <mekpenyong2@gmail.com>
2022-06-18 22:07:00 +0100

    refactored

--- I4G01517324E/urls.py
+++ I4G01517324E/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("django_template.urls"))
+    path('', include("blog.urls"))
 ]
--- blog/apps.py
+++ blog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoTemplateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_template'
+    name = 'blog'
